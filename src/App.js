85b3d67e1ce3a47d9cba330e37ab50acae57eded
snmadhura <snmadhura@gmail.com>
2018-06-06 15:44:21 +0530

    flexible list- big drama

--- src/App.js
+++ src/App.js
@@ -14,13 +14,20 @@ state = {
 }
 
 
-nameChangedHandler = (event) => {
+nameChangedHandler = (event,id) => {
+const personIndex = this.state.person.findIndex(p =>{
+  return p.id === id;
+})
+  
+const persons = {...this.state.person[personIndex]};
+
+persons.name = event.target.value;
+
+const person = [...this.state.person]
+person[personIndex] = persons;
+
 this.setState({
-  person:[
-    {name: 'Madhura' , age:99},
-    {name: event.target.value, age: 54},
-    {name: 'Atal', age: 90}
-  ]
+  person: person
 })
 }
 
@@ -55,7 +62,8 @@ togglePersonHandler = () =>{
             click={() => this.deletePersonHandler(index)}
             name={person.name} 
             age={person.age}
-            key={person.id}/>
+            key={person.id}
+            changed={(event) =>this.nameChangedHandler(event, person.id)}/>
             })
           }
         </div>
